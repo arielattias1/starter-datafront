2cc07e65de2a6ce89422393b98045813f12e3fbe
arielattias1 <74975408+arielattias1@users.noreply.github.com>
2022-02-09 22:09:34 -0500

    Add files via upload

---
+++ working_with_data/02_filter.js
@@ -0,0 +1,11 @@
+var list = [5,8,9,2,7,6,3,1,4];
+
+function callback(item){
+    // return true for included items
+    return item>5;
+}
+
+var filtered = list.filter(callback);
+
+var filtered = filter(list,callback);
+console.log(filtered);
